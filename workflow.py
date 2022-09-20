b30effd998c2ff488573e6d928b226edecb21009
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2022-09-20 15:55:37 +0300

    Add files via upload

---
+++ workflow.py
@@ -0,0 +1,21 @@
+from kfp import dsl
+from mlrun.platforms import auto_mount
+import os
+import sys
+import mlrun
+
+funcs = {}
+
+# init functions is used to configure function resources and local settings
+def init_functions(functions: dict, project=None, secrets=None):
+    for f in functions.values():
+        f.apply(auto_mount())
+
+@dsl.pipeline(
+    name="Testing Conditions",
+    description="Testing Conditions"
+)
+
+def kfpipeline():
+    
+    serving = funcs['serving'].deploy_step()
