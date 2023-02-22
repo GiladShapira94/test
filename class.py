66965d2eea6fdade7d0e1c97b28c1edfcfa56701
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2023-02-22 11:25:10 +0200

    Update class.py

--- class.py
+++ class.py
@@ -1,9 +1,11 @@
 class Test:
   
-    def __init__(self,value):
-        self.name = value
+    def __init__(self,init_value):
+        self.init_value = init_value
     
-    def get_name(self):
-        name = self.name
-        return name
+    def get_init_value(self,param_value):
+        init_value = self.init_value
+        param_value = param_value
+        d = {"init_value":init_value,"param_value":param_value}
+        return d
         
